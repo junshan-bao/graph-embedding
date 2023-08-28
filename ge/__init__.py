70f088262ef1e6faeb224662a1b21aa9085f3f6d
Junshan Bao <junshan@squareup.com>
2023-08-28 22:45:48 +0800

    change log config

--- ge/__init__.py
+++ ge/__init__.py
@@ -2,13 +2,16 @@ import sys
 import logging
 
 
-file = open('log.txt', encoding="utf-8", mode="w")
-logging.basicConfig(
-    stream=file,
-    datefmt='%d-%m-%Y %H%M%S',
-    format='%(asctime)s %(name)s: %(levelname)s: %(message)s',
-    level=logging.INFO
-)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.StreamHandler(open('log.txt', encoding="utf-8", mode="w"))
+file_handler.setFormatter(formatter)
+
 logger = logging.getLogger('ge')
+logger.setLevel(logging.INFO)
 if len(logger.handlers) == 0:
-    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.addHandler(stdout_handler)
+    logger.addHandler(file_handler)
